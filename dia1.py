047b4e658db7566ae307c2b319dc50eaaafa1f0a
Uxitus <uxitus@outlook.es>
2026-04-21 16:10:15 -0600

    Día 1: primer programa en Python

---
+++ dia1.py
@@ -0,0 +1,10 @@
+
+#Calculadora de hrs de estudio
+
+dias= 21
+hrsxdia= 4
+total= dias *hrsxdia
+print("el numero de dias que estudio son", dias)
+print ("las horas son", hrsxdia)
+print("en total son", total)
+print("eso equivale a", total/8,"dias completos po' osho olas puñetas")
