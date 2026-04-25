05f7b891978646f4985d3d90ec8735ab102b4938
Uxitus <uxitus@outlook.es>
2026-04-24 23:21:57 -0600

    Día 4: ejercicio extra loops resuelto

--- extra4.py
+++ extra4.py
@@ -1,6 +1,8 @@
 numero_de_numeros= int(input("Cuantos numeros quieres sumar?"))
+suma = 0
 
-input("okey, ", numero_de_numeros, " vas a sumar")
 
-for n1 in range(1, numero_de_numeros + 1)
-n
+for n1 in range(1, numero_de_numeros + 1):
+      numero= int(input("Número " + str(n1) +  ": "))
+      suma = suma + numero
+print ("La suma total es: " , suma)
